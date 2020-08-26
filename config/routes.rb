8ec6de0f1c1dfd0bcc0bacd82ed5d43af272a257
vagrant <vagrant@localhost.localdomain>
2020-08-26 12:12:56 +0000

    [add]device

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+  sessions:      'users/sessions',
+  passwords:     'users/passwords',
+  registrations: 'users/registrations'
+}
+  devise_for :tour_providers, controllers: {
+  sessions:      'tour_providers/sessions',
+  passwords:     'tour_providers/passwords',
+  registrations: 'tour_providers/registrations'
+}
+
+  root to: 'homes#top'
+  get "home/about" => "homes#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
